87daece5f5927ac36b4c8b24f293570e30fbf4fa
Angmar3019 <info@vockensohn.de>
2024-01-15 09:48:50 +0100

    Add modules

---
+++ modules/hough_transformation.py
@@ -0,0 +1,3 @@
+def hough_transformation(logger, cv, frame):
+    logger.info("Detection via hough transformation is used")
+    pass
---
+++ modules/tensorflow.py
@@ -0,0 +1,6 @@
+def tensorflow(logger, cv, frame):
+
+
+    value = 12.40
+
+    return value
